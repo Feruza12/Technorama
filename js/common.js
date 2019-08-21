9d71d1c17775b2c95fb797805f4a69cf96ef7e02
Feruza12 <f.latipova@student.inha.uz>
2019-08-21 21:07:39 +0500

    toggle added

--- js/common.js
+++ js/common.js
@@ -1,21 +1,36 @@
-$(function() {
+$(function () {
 
 	$(document).ready(function () {
-		// $("#sidebar").mCustomScrollbar({
-		//   theme: "minimal"
-		// });
-  
 		$('#dismiss, .overlay').on('click', function () {
-		  $('#sidebar').removeClass('active');
-		  $('.overlay').removeClass('active');
+			$('#sidebar').removeClass('active');
+			$('.overlay').removeClass('active');
 		});
-  
-		$('#sidebarCollapse').on('click', function () {
-		  $('#sidebar').addClass('active');
-		  $('.overlay').addClass('active');
-		  $('.collapse.in').toggleClass('in');
-		  $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-		});
-	  });
 
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').addClass('active');
+			$('.overlay').addClass('active');
+			$('.collapse.in').toggleClass('in');
+			$('a[aria-expanded=true]').attr('aria-expanded', 'false');
+		});
+	});
+
+	// var switchStatus = false;
+	// $(".wrap").on('change', function () {
+	// 	if ($(this).is(':checked')) {
+	// 		switchStatus = $(this).is(':checked');
+	// 		// alert(switchStatus);// To verify
+	// 	}
+	// 	else {
+	// 		switchStatus = $(this).is(':checked');
+	// 		// alert(switchStatus);// To verify
+	// 	}
+	// });
+
+	// if(switchStatus){
+	// 	document.documentElement.classList.add('theme--dark')
+	// }
+	// else{
+	// 	document.documentElement.classList.add('theme--default')
+	// }
+	
 });
